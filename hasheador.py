5468fb2f0921ea4c49a1227213522ae246afa75c
rodrvn <113926247+rodrvn@users.noreply.github.com>
2023-02-10 14:29:49 -0300

    configuration added
    backend

--- hasheador.py
+++ hasheador.py
@@ -1,26 +1,36 @@
 import hashlib
+import db
 
-# r: row, sirve para que lea el '/' como string y no como caracter de escape 
-
-path1=r'C:\Users\Hp\Desktop\proyecto\subidas\shrek.jpg'
-path2=r'C:\Users\Hp\Desktop\proyecto\subidas\shrek.jpg'
-
-# rb= r de reading mobe b de binary mode
-
-def hasheador(path):
+def hasheado(path):
     with open(path, 'rb') as opened_file:
-        content= opened_file.read() #read lee todo el contenido
-        sha256= hashlib.sha256()
+        content = opened_file.read()
+        sha256 = hashlib.sha256()
 
         sha256.update(content)
+        # print('El hash de la imagen es: ')
+        return ('{} : {}'.format(sha256.name, sha256.hexdigest()))
 
-        print('el hash de la imagen es: ')
-        print()
-        print('{}:{}'.format(sha256.name, sha256.hexdigest()))
+def comparador(path1, path2):
+    hash1 = hasheado(path1)
+    hash2 = hasheado(path2)
 
-hash1=hasheador(path1)
-hash2=hasheador(path2)
-
-if hash1==hash2:
-    print()
-    print('los hashes son iguales')
+    if hash1 == hash2:
+        return True
+    else:
+        return False
+    
+def url_select(path):
+    if comparador(db.path_cortada, path):
+        return ('info_cortada.html')
+    elif comparador(db.path_quemadura, path):
+        return ('info_quemadura.html')
+    elif comparador(db.path_congela, path):
+        return ('info_congela.html')
+    elif comparador(db.path_mordedura, path):
+        return ('info_mordedura.html')
+    elif comparador(db.path_respiracion, path):
+        return ('info_respiracion.html')
+    elif comparador(db.path_arma, path):
+        return ('info_arma.html')
+    else:
+        return ('vayavaya.html')
